5f433f2fdacedb94474c44b2f15794ad47e33cd2
Elisa Occhipinti <elisa_occhipinti@live.it>
2022-11-12 11:24:20 +0100

    remove node_modules

--- wefox-challenge/src/app/app.module.ts
+++ wefox-challenge/src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { GoogleMapsModule } from '@angular/google-maps'
 import {MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MapComponent } from './components/map/map.component';
 import { PostComponent } from './components/post/post.component';
 import { PostCollectionComponent } from './components/post-collection/post-collection.component';
 import { PopupComponent } from './components/popup/popup.component';
@@ -14,16 +11,13 @@ import { PopupComponent } from './components/popup/popup.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent,
     PostComponent,
     PostCollectionComponent,
     PopupComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    GoogleMapsModule,
     MatCardModule
   ],
   providers: [],
